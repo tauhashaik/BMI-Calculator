bb4471cc657dd0699cf2fd9622292f2ba052e8cb
tauhashaik <hoosains19@gmail.com>
2023-11-13 16:50:55 +0200

    calucation complete

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,11 +2,12 @@ let height = document.querySelector('[data-height]').value
 let weight = document.querySelector('[data-weight]').value
 let display = document.querySelector('[data-answer]')
 let button = document.querySelector('[data-calculate]')
+button.addEventListener('click', calculate)
 
 function calculate(){
-    let height = document.querySelector('[data-height]').value
-    let weight = document.querySelector('[data-weight]').value
-    let answer = weight/(height**2)
+    let height = document.querySelector('[data-height]').value;
+    let weight = document.querySelector('[data-weight]').value;
+    let answer = weight / (height**2)
     console.log(answer)
     display.value = answer
 }
